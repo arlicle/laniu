d882db507b7c455316bb7d585f5b3a0a34a84e6c
edison <arlicle@gmail.com>
2018-12-31 14:18:28 +0800

    fix *

--- src/laniu/core.clj
+++ src/laniu/core.clj
@@ -8,81 +8,73 @@
   A string field, for small- to large-sized strings.
   For large amounts of text, use text-field.
   "
-  [& opts]
-  (let [opts-map (apply hash-map opts)
-        max-length-spec (if-let [max-length (:max-length opts-map)]
-                          #(<= (count %) max-length))
-        choices-spec (if-let [choices (:choices opts-map)]
+  [opts]
+  (let [
+        max-length-spec (if-let [max-length (:max-length opts)]
+                          `#(<= (count %) ~max-length))
+        choices-spec (if-let [choices (:choices opts)]
                        (let [choices-map (into {} choices)]
-                         #(contains? choices-map %)))
+                         `#(contains? ~choices-map %)))
         ]
 
-    {
-     :valid-spec (filter identity [string? max-length-spec choices-spec])
-     :options    opts-map
-     })
+    (filterv identity [string? max-length-spec choices-spec])
+    )
   )
 
 
-(defn- integer-field
+(defn- int-field
   "
   An integer. Values from -2147483648 to 2147483647 are safe in all databases.
   "
-  [& opts]
-  (let [opts-map (apply hash-map opts)
-        max-value (:max-value opts-map)
-        min-value (:min-value opts-map)
+  [opts]
+  (let [
+        max-value (:max-value opts)
+        min-value (:min-value opts)
 
-        ;max-value (if (and max-value (> max-value 2147483647)) 2147483647 max-value)
-        ;min-value (if (and min-value (< min-value -2147483648)) -2147483648 min-value)
+        max-value (if (and max-value (> max-value 2147483647)) 2147483647 max-value)
+        min-value (if (and min-value (< min-value -2147483648)) -2147483648 min-value)
 
         max-value-spec (if max-value
-                         #(<= % max-value))
+                         `#(<= % ~max-value))
         min-value-spec (if min-value
-                         #(>= % min-value))
+                         `#(>= % ~min-value))
 
-        choices-spec (if-let [choices (:choices opts-map)]
+        choices-spec (if-let [choices (:choices opts)]
                        (let [choices-map (into {} choices)]
-                         #(contains? choices-map %)))
-        validator-spec (if-let [validator (:validator opts-map)]
-                         validator)
+                         `#(contains? ~choices-map %)))
         ]
 
-    {
-     :valid-spec (filter identity [int? max-value-spec min-value-spec choices-spec validator-spec])
-     :options    opts-map
-     })
+    (filterv identity [int? max-value-spec min-value-spec choices-spec])
+    )
   )
 
-(defn- tiny-integer-field
+
+(defn- tiny-int-field
   "
   Like an integer-field, but only allows values under a certain (database-dependent) point.
   Values from -128 to 127 are safe in all databases.
   "
-  [& opts]
-  (let [opts-map (apply hash-map opts)
-        max-value (:max-value opts-map)
-        min-value (:min-value opts-map)
+  [opts]
+  (let [
+        max-value (:max-value opts)
+        min-value (:min-value opts)
 
-        ;max-value (if (and max-value (> max-value 127)) 127 max-value)
-        ;min-value (if (and min-value (< min-value -128)) -128 min-value)
+        max-value (if (and max-value (> max-value 127)) 127 max-value)
+        min-value (if (and min-value (< min-value -128)) -128 min-value)
 
         max-value-spec (if max-value
-                         #(<= % max-value))
+                         `#(<= % ~max-value))
         min-value-spec (if min-value
-                         #(>= % min-value))
+                         `#(>= % ~min-value))
 
-        choices-spec (if-let [choices (:choices opts-map)]
+        choices-spec (if-let [choices (:choices opts)]
                        (let [choices-map (into {} choices)]
-                         #(contains? choices-map %)))
-        validator-spec (if-let [validator (:validator opts-map)]
-                         validator)
+                         `#(contains? ~choices-map %)))
         ]
 
-    {
-     :valid-spec (filter identity [int? max-value-spec min-value-spec choices-spec validator-spec])
-     :options    opts-map
-     })
+
+    (filterv identity [int? max-value-spec min-value-spec choices-spec])
+    )
   )
 
 
@@ -103,14 +95,41 @@
 
   (let [first-arg (first args)
         doc-string (if (string? first-arg) first-arg)
-        fields-config (eval (if (string? first-arg) (second args) first-arg))
-        fields-keys (keys fields-config)
+        fields-configs (eval (if (string? first-arg) (second args) first-arg))
+        fields-keys (keys fields-configs)
+        name-space "laniu."
+        {req-fields :req opt-fields :opt}
+        (reduce (fn [r [k v]]
+                  (if (or (= :auto-field (:type v)) (:default v))
+                    (update-in r [:opt] conj k)
+                    (update-in r [:req] conj k)
+                    )
+                  ) {:req [] :opt []} fields-configs)
         ]
-    `(let [fields-config# ~fields-config fields-keys# ~fields-keys]
 
+    `(do
+       ~@(for [[k field-opts] fields-configs]
+           `(s/def
+              ~(keyword (str name-space (name model-name)) (name k))
+              (s/and
+                ~@(case (:type field-opts)
+                    :char-field
+                    (char-field field-opts)
+
+                    :int-field
+                    (int-field field-opts)
+
+                    :tiny-int-field
+                    (tiny-int-field field-opts)
+                    )
+                )
+              ))
+
+       (s/def ::user
+         (s/keys :req-un ~req-fields
+                 :opt-un ~opt-fields
+                 ))
        )
-
-
     )
   )
 
@@ -169,3 +188,15 @@
 
 
 (insert user {:first-name "hello" :last-name "nihao" :gender 1})
+
+(defmodel user
+          "define a model"
+          {
+           :first-name {:type :char-field :verbose-name "First name" :max-length 30}
+           :last-name  {:type :char-field :verbose-name "Last name" :max-length 30}
+           :gender     {:type :tiny-int-field :verbose-name "Gender" :choices [[1 "Male"] [2 "Female"]] :default 0}
+           :created    {:type :int-field :verbose-name "Created" :default #(quot (System/currentTimeMillis) 1000)}
+           }
+          )
+
+(s/valid? (keyword "laniu.user" "first-name") "hello")
