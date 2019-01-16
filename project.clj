2bff7a6e1a7e0a94ee3de0ab737799ec8fcffede
edison <arlicle@gmail.com>
2019-01-16 21:56:56 +0800

    version 0.1.6.8

--- project.clj
+++ project.clj
@@ -1,8 +1,6 @@
 (defproject laniu "0.1.6.8-SNAPSHOT"
   :description "Laniu can help you rapid development and clean.
-
   It’s django model for clojure. \n\n
-
   A Clojure library designed to normal human that don't like SQL, well, if you don't like SQL , that part is up to you.
   "
   :url "https://github.com/arlicle/laniu"
