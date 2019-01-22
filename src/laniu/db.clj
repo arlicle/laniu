6d2e26717412316db228e0d90fad0ca958b83f3a
edison <arlicle@gmail.com>
2019-01-22 09:17:36 +0800

    Fix *

--- src/laniu/db.clj
+++ src/laniu/db.clj
@@ -6,29 +6,55 @@
           :fields {:name {:type :char-field :max-length 60}}
           :meta {:db_table "ceshi_publisher"})
 
+(defmodel Category
+          :fields {:name       {:type :char-field :max-length 30}
+                   :sort_order {:type :int-field :default 0}}
+          :meta {:db_table "ceshi_category"})
+
+(defmodel reporter
+          :fields {:full_name {:type :char-field :max-length 70}}
+          :meta {:db_table "ceshi_reporter"})
+
+(defmodel category
+          :fields {:name       {:type :char-field :max-length 30}
+                   :sort_order {:type :int-field :default 0}}
+          :meta {:db_table "ceshi_category"})
+
+(defmodel article
+          :fields {:headline   {:type :char-field :max-length 200}
+                   :content    {:type :text-field}
+                   :view_count {:type :int-field :default 0}
+                   :reporter   {:type :foreignkey :model reporter :on-delete :cascade}
+                   :category   {:type :foreignkey :model category :on-delete :set-null :blank true}
+                   :created    {:type :int-field :default #(quot (System/currentTimeMillis) 1000)}}
+          :meta {:db_table "ceshi_article"})
+
+
 
 (defn field-to-column
-  [[k v]]
+  [model [k v]]
   (str
     "`"
     (case (:type v)
       :auto-field
-      (get-field-db-column Publisher k)
+      (get-field-db-column model k)
 
       :char-field
-      (get-field-db-column Publisher k)
+      (get-field-db-column model k)
 
-      :else
+      ; others
+      (get-field-db-column model k)
       )
     "`"
     ))
 
+(field-to-column Category [:sort_order {:db_column "sort_order", :type :int-field, :default 0}])
 
 (defn get-field-type
   [[k v]]
   (str
     (case (:type v)
-      :auto-field
+      (:auto-field :int-field :foreignkey)
       "int(11)"
 
       :char-field
@@ -78,7 +104,7 @@
                 (str r " " s)
                 r))
             [
-             (field-to-column item)
+             (field-to-column model item)
              (get-field-type item)
              (null-field item)
              (auto-increment item)]))
@@ -87,14 +113,16 @@
 
 (partial add-primary-key model)
 
-(prn (fields-to-db-info Publisher))
+(prn (fields-to-db-info Category))
 
 (defn create-table
   [model]
   (let [model-db-name (get-model-db-name model)]
-    (str "CREATE TABLE `ceshi_reporter` (\n"
+    (str "CREATE TABLE `" model-db-name "` (\n"
          (clojure.string/join ",\n" (fields-to-db-info model))
          "\n) ENGINE=InnoDB DEFAULT CHARSET=utf8"
          )))
 
 (create-table Publisher)
+(create-table Category)
+(create-table article)
